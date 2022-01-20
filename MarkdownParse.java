ac754a2a176e35da39d1b82c580256204a267c50
abijitj <97484194+abijitj@users.noreply.github.com>
2022-01-20 12:37:05 -0800

    Fixed logical error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,18 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket == -1){
+                break; 
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            
+            if(markdown.substring(nextOpenBracket + 1, nextCloseBracket).equals("Link")){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
-        }
+        }        
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
