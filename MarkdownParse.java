eee009fffbb29f1dd1ee2647ae34987e0f95ae38
abijitj <97484194+abijitj@users.noreply.github.com>
2022-01-20 12:46:20 -0800

    Fixed logical error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,13 +18,12 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            //toReturn.add(markdown.substring(openParen + 1, closeParen));
 
-            /* FIX
+            //FIXED VERSION
             if(markdown.substring(nextOpenBracket + 1, nextCloseBracket).equals("Link")){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
-            */
             currentIndex = closeParen + 1;
         }        
         return toReturn;
