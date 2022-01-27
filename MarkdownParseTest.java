8a2d14a4ec22911edd689505fa5bd0cfe8f5bf9b
abijitj <97484194+abijitj@users.noreply.github.com>
2022-01-27 12:42:38 -0800

    Created junit tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,10 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +12,43 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    
+    @Test
+    public void testGetLinks(){
+        ArrayList<String> expected = new ArrayList<>(); 
+        assertEquals("Tests when there are no links", expected, MarkdownParse.getLinks("test2.md")); 
+
+        ArrayList<String> expected2 = new ArrayList<>();
+        expected2.add("https://www.newlink.com"); 
+        expected2.add(" ");
+
+        //String fileContents = 
+
+        assertEquals("When there is one link and one ()[ ]", 
+        expected2, MarkdownParse.getLinks(" # Test file [Link](https://www.newlink.com)
+        ![Image](https://animals.net/wp-content/uploads/2019/01/Water-Dragon-5.jpg)'''[]( )''' ")); 
+
+        try {
+            assertEquals("When there is one link and one ()[ ]", 
+            expected2, MarkdownParse.getLinks(Files.readString(Path.of("test.md"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            assertEquals expected2, MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    public void testPrint(){
+        ArrayList<String> expected2 = new ArrayList<>();
+        expected2.add("https://www.newlink.com"); 
+        expected2.add(" ");
+ 
+        assertPrint("[https://www.newlink.com, ]", System.out.println(expected2));
+    }
+    */
 }
