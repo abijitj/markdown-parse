78e502e69c1b09f43518279ff63d831f69ac607f
abijitj <97484194+abijitj@users.noreply.github.com>
2022-02-25 15:01:47 -0800

    Lab 4 Complete

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.*;
 
 public class MarkdownParseTest {
+    /*
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -27,15 +29,38 @@ public class MarkdownParseTest {
             expected2, MarkdownParse.getLinks(Files.readString(Path.of("test.md"))));
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }  
+    }
 
-        /*
-        try {
-            assertEquals(expected2, MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        */
+    @Test
+    public void getLinks1() throws IOException{
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    } */
+    @Test
+    public void getLinks8() throws IOException{
+        List<String> expect = List.of("`google.com","google.com","ucsd.edu");
+        Path fileName = Path.of("snippet-1.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void getLinks9() throws IOException{
+        List<String> expect = List.of("a.com","a.com(())","example.com");
+        Path fileName = Path.of("snippet-2.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void getLinks10() throws IOException{
+        List<String> expect = List.of("https://www.twitter.com","https://ucsd-cse15l-w22.github.io/","https://cse.ucsd.edu");
+        Path fileName = Path.of("snippet-1.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     /*
