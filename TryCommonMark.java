c6a73e9920e70172b7ab0d89ab48ff8c1f429178
abijitj <97484194+abijitj@users.noreply.github.com>
2022-03-10 16:46:16 -0800

    Created URLVisitor2

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -11,6 +13,23 @@ class URLVisitor extends AbstractVisitor {
 
         // Count words (this is just an example, don't actually do it this way for various reasons).
         wordCount += text.getLiteral().split("\\W+").length;
+
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(text);
+    }
+}
+
+class URLVisitor2 extends AbstractVisitor {    
+    int linkCount = 0;
+    ArrayList<String> links; 
+
+    @Override
+    public void visit(Link text) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        //linkCount += text.getDestination().split("\\W+").length;
+        links.add(text.getDestination());
 
         // Descend into children (could be omitted in this case because Text nodes don't have children).
         visitChildren(text);
